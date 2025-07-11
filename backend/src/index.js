a919d8d2f7312fd361f1b56735d6244a49dc4ef5
anujs15 <anujsaraswat9027@gmail.com>
2025-07-11 17:46:29 +0530

    final

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const cors = require('cors')
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://code-craft-lovat.vercel.app'],
     credentials: true 
 }))
 
